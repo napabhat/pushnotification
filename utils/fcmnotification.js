150a91134587112d7adf6066c01634fb1f65f2f7
kengbascher <keng_bascher@hotmail.com>
2016-07-15 18:11:59 +0700

    success push.

--- utils/fcmnotification.js
+++ utils/fcmnotification.js
@@ -23,37 +23,25 @@ var message = new fcm.Message({
     notification: {
         title: "NodeJs",
         icon: "ic_launcher",
-        body: "Test Send Notification"
+        body: "Node send notification"
     }
 });
 
-// function retrieveVertById(id, successCallback) {
-//
-//     dbCon.tokens.find({
-//         token: id
-//     }, function (err, clusters) {
-//         if (err || !clusters) {
-//             console.log("No cluster found");
-//         } else {
-//             regTokens = [];
-//             clusters.forEach(function (cluster) {
-//                 vert = cluster["vertices"];
-//                 regTokens.push(vert);
-//             });
-//             successCallback(regTokens);
-//         }
-//     });
-// };
-//
-// retrieveVertById("d_B8-wedUMI:APA91bHIuMgGuVbcWqngctaduQLq7TkHRDXV-v8zb79DwvNmM_OiZtp3OyJvp7AN66JDcn0P8OIuz14wOAGimG_S0Hvy9gwYS66gGJelULD-yIZknPI2cACu_Om0kxW9xpbsULXzukFu", function(result) {
-//     console.log(result);
-// });
+
+    dbCon.tokens.find(function (err, tokens) {
+        if (err) return next(err);
+        tokens.forEach(function (tokensObject) {
+            regTokens.push(tokensObject.token);
+        })
+        console.log('token :' + tokens);
+    });
+
 
 //Add your mobile device registration tokens here
-var regTokens = ['d_B8-wedUMI:APA91bHIuMgGuVbcWqngctaduQLq7TkHRDXV-v8zb79DwvNmM_OiZtp3OyJvp7AN66JDcn0P8OIuz14wOAGimG_S0Hvy9gwYS66gGJelULD-yIZknPI2cACu_Om0kxW9xpbsULXzukFu',
-                'eIGilhdXaCU:APA91bGYAopM45dV7L4pzXtaNqk61FVGSxnvNq3shCEk04JUWqfa363R15jvVw9s3PDoq7wSPGNsW-8iIslyE757Oqrz18rG7KKraPGF5M3rmLOBCHp2pTKQ8Xc-D633L7c3agwzxapd'];
 
-
+// var regTokens = ['d_B8-wedUMI:APA91bHIuMgGuVbcWqngctaduQLq7TkHRDXV-v8zb79DwvNmM_OiZtp3OyJvp7AN66JDcn0P8OIuz14wOAGimG_S0Hvy9gwYS66gGJelULD-yIZknPI2cACu_Om0kxW9xpbsULXzukFu',
+//                 'eIGilhdXaCU:APA91bGYAopM45dV7L4pzXtaNqk61FVGSxnvNq3shCEk04JUWqfa363R15jvVw9s3PDoq7wSPGNsW-8iIslyE757Oqrz18rG7KKraPGF5M3rmLOBCHp2pTKQ8Xc-D633L7c3agwzxapd',
+//                 'eIGilhdXaCU:APA91bGYAopM45dV7L4pzXtaNqk61FVGSxnvNq3shCEk04JUWqfa363R15jvVw9s3PDoq7wSPGNsW-8iIslyE757Oqrz18rG7KKraPGF5M3rmLOBCHp2pTKQ8Xc-D633L7c3agwzxapd'];
 
 //Replace your developer API key with GCM enabled here
 var sender = new fcm.Sender('AIzaSyAb1YKvsZ89k-w85Mk0F6g8lNq49Daqq-A');
